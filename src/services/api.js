d1e1f593f01215ca10f7b91aab06e0cd32ea154c
berner <maciasberner@gmail.com>
2026-06-09 20:39:24 -0500

    Correccion Backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: 'https://ordenes-backend-cy57.onrender.com', // Cambia esto por la URL de tu backend
 });
 
 // Interceptor: Antes de cada petición, revisa si hay un token y lo adjunta
